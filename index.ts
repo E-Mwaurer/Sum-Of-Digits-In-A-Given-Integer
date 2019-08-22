77c9d55dcf107a9a67d745c9f5e3b896205b4fe7
EdwardMwaura <edduemwaurer@outlook.com>
2019-08-22 18:15:11 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -5,11 +5,10 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>Algorithm Challenge 4</h1>`;
 function addTwoDigits(n: any): number {
-  const stringified = n.toString();
-  const v = stringified.split('');
+  const stringified = n.toString().split('');
   const values = [];
-  for (let i = 0; i < v.length; i++) {
-    values.push(parseInt(v[i]));
+  for (let i = 0; i < stringified.length; i++) {
+    values.push(parseInt(stringified[i]));
   }
   const sum = values.reduce((acc, val) => acc + val, 0);
   return sum;
